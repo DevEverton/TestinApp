33676507db382a294210f70d5901414adff51500
Everton Carneiro <everton.mercante@gmail.com>
2022-03-16 21:37:13 -0300

    Test 2

--- TestApp/ViewController.swift
+++ TestApp/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        print("Just testing")
+        print("Just testing2")
     }
 
 
